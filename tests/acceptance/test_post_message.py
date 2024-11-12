b478d6b7ba0ea0e130b6aa9c0ab6c98c36c02dd3
Duncan Rickelton <duncan.rickelton@artlogic.net>
2024-11-12 12:37:44 +0000

    TestPostMessage acceptance test (red)

--- tests/acceptance/test_post_message.py
+++ tests/acceptance/test_post_message.py
@@ -1,3 +1,21 @@
+from social_network_kata.interface import Interface
+from social_network_kata.printer import Printer
+
+
 class TestPostMessage:
-    def test_user_can_post_message_to_timeline(self):
-        assert True
+    """"
+    > Alice -> I love the weather today
+    Alice can publish messages to a personal timeline.
+    > Bob -> Damn! We lost!
+    > Bob -> Good game though.
+    """
+    def test_user_can_post_and_receives_success_message(self):        
+        printer = Printer()
+        interface = Interface(printer = printer)
+        user = "Atta" # Alice
+        post_contents = "The caterpillar's using himself as live bait"
+        command = Command(f'{user} -> {post_contents}')
+        
+        interface.send_command(command)
+        
+        printer.print.assert_called_once_with(f'Succesfully posted: {post_contents}')
