f764fdda1b467ae09317b0dd279fb66bdfd907da
Duncan Rickelton <duncan.rickelton@artlogic.net>
2024-11-12 13:05:54 +0000

    tweak acceptance test

--- tests/acceptance/test_post_message.py
+++ tests/acceptance/test_post_message.py
@@ -1,6 +1,13 @@
+from social_network_kata.command import Command
 from social_network_kata.interface import Interface
 from social_network_kata.printer import Printer
 
+ALICE = "PrincessAtta"
+ALICE_POST = "The caterpillar's using himself as live bait"
+BOB = "Slim"
+BOB_POST = "I'm the only stick with eyeballs!"
+CHARLIE = "Heimlich"
+CHARLIE_POST = "Someday, I will be a beautiful butterfly, and then everything will be better." 
 
 class TestPostMessage:
     """"
@@ -12,10 +19,15 @@ class TestPostMessage:
     def test_user_can_post_and_receives_success_message(self):        
         printer = Printer()
         interface = Interface(printer = printer)
-        user = "Atta" # Alice
-        post_contents = "The caterpillar's using himself as live bait"
-        command = Command(f'{user} -> {post_contents}')
-        
-        interface.send_command(command)
+        user = ALICE
+        post_contents = ALICE_POST
+        command = f'{user} -> {post_contents}'
+
+        terminal.call(
+            "AntySocialNetwork()", # Anty: post something.
+            f"{command}"
+        )
+
+        subprocess.getoutput() # something like this to input text to terminal
         
         printer.print.assert_called_once_with(f'Succesfully posted: {post_contents}')
