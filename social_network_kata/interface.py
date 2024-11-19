801ce2ef0b7e5de6c96c44a2340cea5ddde014a5
Damian Haziak <dmnhaziak3@gmail.com>
2024-11-19 11:49:12 +0000

    refactored acceptance test to use patch
    Co-authored-by: Alexandre H. Saad <alexandresaad@icloud.com>
    Co-authored-by: ArtlogicDuncan <ArtlogicDuncan@users.noreply.github.com>

--- social_network_kata/interface.py
+++ social_network_kata/interface.py
@@ -1,2 +1,2 @@
-class Interface:
+class SocialNetworkCLI:
     pass
--- tests/acceptance/test_post_message.py
+++ tests/acceptance/test_post_message.py
@@ -1,5 +1,6 @@
-from social_network_kata.command import Command
-from social_network_kata.interface import Interface
+from unittest.mock import patch
+
+from social_network_kata.interface import SocialNetworkCLI
 from social_network_kata.printer import Printer
 
 ALICE = "PrincessAtta"
@@ -10,24 +11,11 @@ CHARLIE = "Heimlich"
 CHARLIE_POST = "Someday, I will be a beautiful butterfly, and then everything will be better." 
 
 class TestPostMessage:
-    """"
-    > Alice -> I love the weather today
-    Alice can publish messages to a personal timeline.
-    > Bob -> Damn! We lost!
-    > Bob -> Good game though.
-    """
-    def test_user_can_post_and_receives_success_message(self):        
+    @patch('builtins.input', side_effect=["PrincessAtta -> The caterpillar's using himself as live bait"])
+    def test_user_can_post_and_receives_success_message(self, _):        
         printer = Printer()
-        interface = Interface(printer = printer)
-        user = ALICE
-        post_contents = ALICE_POST
-        command = f'{user} -> {post_contents}'
-
-        terminal.call(
-            "AntySocialNetwork()", # Anty: post something.
-            f"{command}"
-        )
-
-        subprocess.getoutput() # something like this to input text to terminal
+        social_network_cli = SocialNetworkCLI(printer=printer)
         
-        printer.print.assert_called_once_with(f'Succesfully posted: {post_contents}')
+        social_network_cli.run()
+        
+        printer.print.assert_called_once_with(f"Succesfully posted: PrincessAtta -> The caterpillar's using himself as live bait")
