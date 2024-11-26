4ed10fdb3c8a92a6a2b75828dc15d999468a5e19
Damian Haziak <dmnhaziak3@gmail.com>
2024-11-26 10:49:08 +0000

    fixed imports in acceptance and unit tests
    Co-authored-by: HarrisonLongartlogic <HarrisonLongartlogic@users.noreply.github.com>
    Co-authored-by: Alexandre H. Saad <alexandresaad@icloud.com>

--- social_network_kata/social_network_cli.py
+++ social_network_kata/social_network_cli.py
@@ -1,6 +1,12 @@
+from social_network_kata.input_wrapper import InputWrapper
+from social_network_kata.printer import Printer
+from social_network_kata.social_network_service import SocialNetworkService
+
 class SocialNetworkCLI:
-    def __init__(self, printer):
+    def __init__(self, social_network_service: SocialNetworkService, printer: Printer, input: InputWrapper):
+        self.social_network_service = social_network_service
         self.printer = printer
+        self.input = input
     
-    def run():
+    def run(self):
         raise NotImplementedError
--- tests/unit/test_cli.py
+++ tests/unit/test_cli.py
@@ -10,7 +10,9 @@ class TestCLI:
         post = Post(user_name="PrincessAtta", content="The caterpillar's using himself as live bait")
         mock_printer = Mock()
         mock_social_network_service = Mock()
-        social_network_cli = SocialNetworkCLI(social_network_service=mock_social_network_service, printer=mock_printer)
+        social_network_cli = SocialNetworkCLI(
+            social_network_service=mock_social_network_service, 
+            printer=mock_printer)
 
         social_network_cli.run()
         
